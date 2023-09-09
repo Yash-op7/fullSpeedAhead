1f096cd9a7fb81c175018214c13db823acc17538
Yash <yashm3375@gmail.com>
2023-09-10 02:50:35 +0530

    Done.

---
+++ temp.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define pq priority_queue
+#define ll long long
+#define MOD 1e9 + 7
+#define all(x) x.begin(), x.end()
+#define rep(i, a, b) for (int i = a; i < b; i++)
