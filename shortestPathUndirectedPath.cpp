164456edac41329d61269844e62d0988370ea1dc
Yash <yashm3375@gmail.com>
2023-09-13 15:45:52 +0530

    Done.

---
+++ shortestPathUndirectedPath.cpp
@@ -0,0 +1,25 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define pq priority_queue
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define vl vector<ll>
+#define vvl vector<vl>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+const ll INF = 1e12;
+const int MOD = 1e9 + 7;
+
+class Solution
+{
+public:
+    vector<int> shortestPath(vector<vector<int>> &edges, int N, int M, int src)
+    {
+        // code here
+    }
+};
