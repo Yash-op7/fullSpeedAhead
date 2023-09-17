3c509c708c038a8247c3aee1801e6680a4313eb0
Yash <yashm3375@gmail.com>
2023-09-17 22:14:18 +0530

    Done.

--- thatQue.cpp
+++ thatQue.cpp
@@ -15,28 +15,34 @@ using namespace std;
 const ll INF = 1e12;
 const int MOD = 1e9 + 7;
 
-int f(int x, int y, vector<string> &maze, vvb &vis, vvi &t){
+int f(int x, int y, vector<string> &maze, vvb &vis, vvi &t)
+{
     int n = vis.size();
     int m = vis[0].size();
-    if(x == n-1 && y == m-1){
+    if (x == n - 1 && y == m - 1)
+    {
         return 0;
     }
-    if(t[x][y] != -1){
+    if (t[x][y] != -1)
+    {
         return t[x][y];
     }
     char c = maze[x][y];
-    int cost = INT_MAX;
-    vi dx = {1, 0, 0, -1};
+    int cost = 1e9;
+    vi dx = {-1, 0, 0, 1};
     vi dy = {0, 1, -1, 0};
     //       U  R   L  D
-    if(c == 'U'){
+    if (c == 'U')
+    {
         int free = 0;
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 4; i++)
+        {
             int tx = x + dx[i];
             int ty = y + dy[i];
-            if(tx < n && tx >=0 && ty < m &&ty >= 0 && !vis[tx][ty]){
+            if (tx < n && tx >= 0 && ty < m && ty >= 0 && !vis[tx][ty])
+            {
                 vis[tx][ty] = true;
-                cost = min(cost, (i == free?0:1) + f(tx, ty, maze, vis, t));
+                cost = min(cost, (i == free ? 0 : 1) + f(tx, ty, maze, vis, t));
                 vis[tx][ty] = false;
             }
         }
@@ -95,4 +101,15 @@ int solve(int A, int B, vector<string> &C)
     vvb vis(A, vb(B, false));
     vis[0][0] = true;
     return f(0, 0, C, vis, t);
+}
+int main(){
+    int n, m;
+
+    cin>>n>>m;
+    vector<string> c(n);
+    getline(cin, c[0]);;
+    for(int i=0;i<n;i++){
+        getline(cin, c[i]);
+    }
+    cout<<solve(n, m, c)<<endl;
 }
