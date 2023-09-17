f3a7324e4c2cabbd5592d03e3f0242ef9932bd5c
Yash <yashm3375@gmail.com>
2023-09-17 20:32:29 +0530

    Done.

---
+++ infekt.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define pq priority_queue
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define vl vector<ll>
+#define vvl vector<vl>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+const ll INF = 1e12;
+const int MOD = 1e9 + 7;
+
+
+int minMalwareSpread(vector<vector<int>>& graph, vector<int>& malware)
+{
+	// Write your code here
+	unordered_map<int , bool> isMalware;
+	for(const auto x:malware){
+		isMalware[x] = true;
+	}
+	int n = graph.size();
+	int ans = 0;
+	int maxCount = 0;
+	for(int i=0;i<malware.size();i++){
+		int currCount = 0;
+		int x = malware[i];
+		vb vis(n, false);
+
+		for(int j=0;j<n;j++){
+			// for the ith malware go to each edge from it
+			currCount = 0;
+			if(graph[x][j] == 1){
+				int branchCnt = 0;
+				bool infected = false;
+				queue<int> q;
+				if(!vis[j]){
+					q.push(j);
+					vis[j] = true;
+					if(isInfected[j])){
+						infected = true;
+						break;
+					}
+					branchCnt++;
+					while(!q.empty()){
+						int top = q.front();
+						q.pop();
+						for(int z=0;z<n;z++){
+							if(graph[top][z] == 1 && !vis[z]){
+								q.push(z);
+								vis[z] = true;
+								if(isInfected[z]){
+									infected=true;
+									break;
+								}
+								branchCnt++;
+							}
+						}
+					}
+				}
+			}
+			if(!infected){
+					currCount += branchCnt;
+				}
+			if(currCount > maxCount){
+				ans = i;
+				maxCount = currCount;
+			}
+		}
+	}
+	return ans;
+	
+}
