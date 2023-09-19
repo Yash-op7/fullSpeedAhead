3c4b1fcd1e2e62ebe8324ada2bb7b6ca2d6cca24
Yash <yashm3375@gmail.com>
2023-09-19 16:37:53 +0530

    Done.

---
+++ her6.cpp
@@ -0,0 +1,26 @@
+int fn(int i, int j, const string &a, const string &b, vector<vector<int>> &dp)
+{
+    if (i < 0 || j < 0)
+        return 0;
+    if (i == 0 && j == 0)
+        return dp[i][j] = 1;
+    if (j == 0)
+        return dp[i][j] = 0;
+    if (dp[i][j] != -1)
+        return dp[i][j];
+    if (a[i - 1] == b[j - 1] || b[j - 1] == '?')
+        return dp[i][j] = fn(i - 1, j - 1, a, b, dp);
+    if (b[j - 1] == '*')
+    {
+        return dp[i][j] = (fn(i, j - 1, a, b, dp) || fn(i - 1, j, a, b, dp));
+    }
+    else
+        return dp[i][j] = 0;
+}
+int Solution::isMatch(const string a, const string b)
+{
+    int n = a.size();
+    int m = b.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+    return fn(n, m, a, b, dp);
+}
