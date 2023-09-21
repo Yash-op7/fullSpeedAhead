098f9c4548d5d324602724b07ecbd84231253368
Yash <yashm3375@gmail.com>
2023-09-22 01:47:14 +0530

    Done.

--- temo.cpp
+++ temo.cpp
@@ -17,27 +17,38 @@ const int MOD = 1e9 + 7;
 
 bool f(int idx, int k, int sum, vi &A, vi &ans, vvvb &t)
 {
-    if(k == 0){
-        if(sum == 0){
+    if (k == 0)
+    {
+        if (sum == 0)
+        {
             return true;
         }
         return false;
     }
     int n = A.size();
-    if(idx == n){
+    if (idx == n)
+    {
         return false;
     }
-    if(!t[idx][k][sum]){
+    if (!t[idx][k][sum])
+    {
         return false;
     }
-    if(A[idx] <= sum){
+    if (A[idx] <= sum)
+    {
+        ans.push_back(A[idx]);
         bool take = f(idx + 1, k - 1, sum - A[idx], A, ans, t);
-        if(take){
-            ans.push_back(A[idx]);
-            return t[idx][k][sum] = true;
+        if (take)
+        {
+            return  true;
         }
+        ans.pop_back();
     }
-    return t[idx][k][sum] = f(idx + 1, k ,sum, A, ans, t);
+    if (f(idx + 1, k, sum, A, ans, t))
+    {
+        return true;
+    }
+    return t[idx][k][sum] = false;
 }
 
 vector<vector<int>> Solution::avgset(vector<int> &A)
@@ -54,23 +65,26 @@ vector<vector<int>> Solution::avgset(vector<int> &A)
     vi ans1;
     vi ans2;
     vvi ans;
-    vvvb t(101, vvb(101, vb(5001, true)));      // 5 x 1e7
+    vvvb t(101, vvb(101, vb(5001, true))); // 5 x 1e7
     for (int i = 1; i < n; i++)
     {
         double temp = avg * i;
         if ((temp - (int)(temp) == 0) && f(0, i, (int)temp, A, ans1, t))
         {
-            for(int j=0;j<ans1.size();j++){
-                int currToRemove = ans1[j];
-                auto it = find(all(A), currToRemove);
-                A.erase(it);
+            int loc = 0;
+            for(int num : ans1){
+                while(loc < n && A[i] != num){
+                    ans2.pus
+                }
             }
-            // ans.push_back(A);
             sort(all(ans1));
-            if(A.size() < ans1.size()){
+            if (A.size() < ans1.size())
+            {
                 ans.push_back(A);
                 ans.push_back(ans1);
-            }else{
+            }
+            else
+            {
                 ans.push_back(ans1);
                 ans.push_back(A);
             }
