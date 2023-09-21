12db13bcfd96cd46433206cc2ac6b237d9aa0564
Yash <yashm3375@gmail.com>
2023-09-21 12:52:27 +0530

    Done.

---
+++ cp/1/equalAverageSubsetPartition.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define pq priority_queue
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define vl vector<ll>
+#define vvl vector<vl>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+const ll INF = 1e12;
+const int MOD = 1e9 + 7;
+
+bool f(int idx, int k, int sum, vi &A, vi &ans)
+{
+    if (k == 0 && sum == 0)
+    {
+        return true;
+    }
+    if (idx == A.size())
+    {
+        return false;
+    }
+    bool take = false;
+    if (A[idx] <= sum)
+    {
+        take = f(idx + 1, k - 1, sum - A[idx], A, ans);
+        if (take)
+        {
+            ans.push_back(A[idx]);
+            return true;
+        }
+    }
+    bool dont = f(idx + 1, k, sum, A, ans);
+    return dont;
+}
+
+vector<vector<int>> avgset(vector<int> &A)
+{
+    int n = A.size();
+    double sum = 0;
+    for (int x : A)
+    {
+        sum += x;
+    }
+
+    double avg = sum / n;
+    vi ans1;
+    vi ans2;
+    vvi ans;
+    for (int i = 0; i <= n; i++)
+    {
+        double temp = avg * i;
+        if ((temp - (int)(temp) == 0) && f(0, i, (int)temp, A, ans1))
+        {
+            sort(all(ans1));
+            ans.push_back(ans1);
+            for (int x : ans1)
+            {
+                A.erase(find(all(A), x));
+            }
+            sort(all(A));
+            ans.push_back(A);
+            return ans;
+        }
+    }
+    return ans;
+}
