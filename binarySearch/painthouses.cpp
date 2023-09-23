a2b8485bf6232327022e75b757f74210c62f3cb1
Yash <yashm3375@gmail.com>
2023-09-23 23:42:18 +0530

    Done.

---
+++ binarySearch/painthouses.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h> 
+const int MOD = 1e8;
+
+int f(int R, int B, int last, int taken, int X, int Y, vector<vector<vector<vector<int>>>> &t){
+    if(R+B == 0){
+        return 1;
+    }
+    if(last == -1){
+        int red = 0;
+        if(X>0 && R>0 && taken < X){
+            // can take red;
+            red = f(R-1, B, 1, 1,X, Y, t)%MOD;
+        }
+        int blue = 0;
+        if(Y>0 && B>0 && taken<Y){
+            blue = f(R, B-1 ,2, 1, X, Y, t)%MOD;
+        }
+        return (red+blue)%MOD;
+    }    
+    else{
+        if(t[R][B][last-1][taken] != -1){
+            // return t[R][B][last-1][taken];
+        }
+        int red =0;
+        int blue = 0;
+        if(last == 1){
+            if(taken < X && R > 0){
+                red += f(R-1, B, 1, taken+1, X, Y, t)%MOD;
+            }
+            if(B>0 && Y>0){
+                red+= f(R, B-1, 2, 1, X, Y, t)%MOD;
+            }
+            return t[R][B][last-1][taken]= red%MOD;
+        }else{
+            if(taken < Y && B > 0){
+                blue += f(R, B-1, 2, taken+1, X, Y, t)%MOD;
+            }
+            if(X>0 && R>0){
+                blue += f(R-1, B, 1, 1, X, Y, t)%MOD;
+            }
+            return t[R][B][last-1][taken]= blue%MOD;
+        }
+    }
+}
+
+int beautifulCity(int N, int R, int B, int X, int Y)
+{
+    // Write your code here.   
+    // vector<vector<vector<vector<int>>>> t(101, 101, 2, 11)
+    vector<vector<vector<vector<int>>>> t(101,
+     vector<vector<vector<int>>>(101, vector<vector<int>>(2, vector<int>(11, -1))));
+    f(R, B, -1, 0, X, Y, t);
+}
---
+++ binarySearch/wordLadder2.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
+        // code here
+    }
+};
