54d920bf3c3847e1b733e260b504f75cf8338577
Yash <yashm3375@gmail.com>
2023-09-24 01:46:57 +0530

    Done.

---
+++ binarySearch/pathWithMinimumMaximumAbsoulteDifferenceInConsecutiveCells.cpp
@@ -0,0 +1,6 @@
+class Solution {
+  public:
+    int MinimumEffort(vector<vector<int>>& heights) {
+        // Code here
+    }
+};
---
+++ minTimeWormholePath.cpp
@@ -0,0 +1,17 @@
+/******************************************************************
+
+    Following is the structure of the Wormhole class:
+
+    class Wormhole
+    {
+    public:
+        int startX, startY, endX, endY, cost;
+    };
+
+*******************************************************************/
+
+
+int minTimeInWormholeNetwork(int n, int sx, int sy, int dx, int dy, Wormhole* wormhole)
+{
+    // Write your Code here
+}
