0a5fc804e2455f0756c69734abd4e728dc64204c
Yash <yashm3375@gmail.com>
2023-09-24 19:16:47 +0530

    Done.

--- AE/endme.cpp
+++ AE/endme.cpp
@@ -1,4 +1,23 @@
-int Solution::findDiceSum(int A, int B, int C)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define vvvb vector<vvb>
+#define pq priority_queue
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define vl vector<ll>
+#define vvl vector<vl>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+const ll INF = 1e12;
+const int MOD = 1e9 + 7;
+
+int findDiceSum(int A, int B, int C)
 {
     // vector<vector<int>>dp(A+1,vector<int>(C+1,0));
     int N = 1e9 + 7;
@@ -10,19 +29,16 @@ int Solution::findDiceSum(int A, int B, int C)
         for (int j = 1; j <= C; j++)
         {
             prev[j] += prev[j - 1];
-            prev[j] = prev[j] % N;
         }
         for (int j = 1; j <= C; j++)
         {
             if (j - B - 1 >= 0)
             {
                 curr[j] = ((prev[j - 1] - prev[j - B - 1]) + N);
-                curr[j] = curr[j] % N;
             }
             else
             {
                 curr[j] = prev[j - 1];
-                curr[j] = curr[j] % N;
             }
         }
         prev = curr;
