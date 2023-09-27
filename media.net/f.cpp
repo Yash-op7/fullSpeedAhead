d5ee63e5cee9972ef0a373f1a69c30e81e56206b
Yash <yashm3375@gmail.com>
2023-09-28 00:28:49 +0530

    Done.

---
+++ media.net/f.cpp
@@ -0,0 +1,49 @@
+
+#define vi vector<int> 
+
+class Solution {
+public:
+    bool f(int idx, int val, int mask, vi &a, vector<vector<vector<bool>>>&t){
+        if(val == 0){
+            return true;
+        }
+        if(idx == a.size()){
+            return false;
+        }
+        if(!t[idx][val][mask]){
+            return false;
+        }
+        if((mask>>idx & 1) == 1){
+            return t[idx][val][mask]= f(idx+1, val, mask, a, t);
+        }
+        if(val >= a[idx]){
+            return t[idx][val][mask]= f(idx+1, val-a[idx], (mask | (1<<idx)), a, t) || f(idx+1, val, mask, a, t);
+        }
+        return t[idx][val][mask]= f(idx+1, val, mask, a, t);
+    } 
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+        }
+        if(sum % k != 0){
+            return false;
+        }
+        int val = sum/k;
+        int mask = 0;
+        vector<vector<vector<bool>>>t(nums.size(), vector<vi>(val+1, vi((1<<16), true)));
+        for(int i=0;i<k-1;i++){
+            if(!f(0, val, mask, nums, t)){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+
+
+
+
