840237b718ed42ba35c96f53c270d3cf1fb5a151
Yash <yashm3375@gmail.com>
2023-10-03 00:12:45 +0530

    Done.

---
+++ bombRaider.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+
+int main(){
+    int n,m,b;
+    cin>>n>>m>>b;
+    vector<vector<char>>maze(n, vector<char>(m, '.'));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            
+        }
+    }
+}
