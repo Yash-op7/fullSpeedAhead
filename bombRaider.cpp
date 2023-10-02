5389ad053d7adee3f6b9a93b14fe294a84e39a31
Yash <yashm3375@gmail.com>
2023-10-03 00:14:26 +0530

    Done.

--- bombRaider.cpp
+++ bombRaider.cpp
@@ -9,9 +9,16 @@ int main(){
     int n,m,b;
     cin>>n>>m>>b;
     vector<vector<char>>maze(n, vector<char>(m, '.'));
+    int x=-1;int y=-1;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            
+            cin>>maze[i][j];
+            if(maze[i][j] == 'S'){
+                x = i;
+                y = j;
+            }
         }
     }
+    vector<vb> vis(n, vb(m, false));
+    return f(x, y, maze, vis, )
 }
