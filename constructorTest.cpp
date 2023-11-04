9d22a7bebaa511010384ea43e48a2b8a813b852a
Yash <yashm3375@gmail.com>
2023-11-04 11:56:52 +0530

    Done.

---
+++ constructorTest.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class animal{
+    public:
+    int x;
+    animal(){
+        cout<<"new object created.\n";
+    }
+};
+
+int main(){
+    animal y;
+
+}
